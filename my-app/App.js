03ec7883484c0e1b60d403eaa82774def1e0648e
Godfred Fiave <fiavegp@gmail.com>
2024-07-06 12:03:58 +0000

    Converted TypeScript files to JavaScript and updated project

---
+++ my-app/App.js
@@ -0,0 +1,27 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>My name is <Text style={styles.boldText}>Godfred Fiave</Text></Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#f0f8ff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    fontSize: 24,
+  },
+  boldText: {
+    fontWeight: 'bold',
+  },
+});
